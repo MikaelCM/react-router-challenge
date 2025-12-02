8d56e689611ebb50a5b2ba6132032824d67d403d
Mikael <mikael.c.mendes@gmail.com>
2025-12-02 09:54:11 -0300

    Ajuste de div do Navbar.tsx para nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,11 @@ import { NavLink } from "react-router-dom"
 
 const Navbar = () => {
   return (
-    <div>
+    <nav>
         <NavLink to='/'>Home</NavLink>
         <NavLink to='/about'>About</NavLink>
         <NavLink to='/end'>End</NavLink>
-    </div>
+    </nav>
   )
 }
 
